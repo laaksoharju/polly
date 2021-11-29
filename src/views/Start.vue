63ce546b8551c5b79fce6d88b21218922b26992b
Filip Fallberg <Filip.Fallberg.5589@student.uu.se>
2021-11-29 13:49:50 +0100

    Filip yallah

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,27 +1,14 @@
 <template>
   <main class="theme">
-<<<<<<< HEAD
   <header>
-    <div>
-      <h1>MapQuiz</h1>
-      <p>Welcome! Get your brain warmed up bishes</p>
-      <button id="LangButton" style  v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
-=======
-  <header class="format">
     <div class="container"><br>
       <h1 id="headertext">MapQuiz</h1>
-      <p>Welcome get your brain warmed up bishes</p>
-      <button id="LangButton" style v-on:click="switchLanguage">{{ uiLabels.changeLanguage }}</button>
->>>>>>> 466be77ed578dc3e8076b6ed3754f193ed08750f
+      <p>Welcome!</p>
+      <button id="LangButton" style  v-on:click="switchLanguage">{{uiLabels.changeLanguage}}</button>
     </div>
   </header>
 
-
-<<<<<<< HEAD
   <section>
-=======
-  <section >
->>>>>>> 466be77ed578dc3e8076b6ed3754f193ed08750f
     <div id="nav">
       <div>
       <button>
@@ -37,13 +24,7 @@
         <router-link v-bind:to="'/create/'+lang">{{ uiLabels.createPoll }}</router-link>
       </button>
     </div>
-<<<<<<< HEAD
-=======
-    <label>
-      Write poll id:
-      <input type="text" v-model="id">
-    </label>
->>>>>>> 466be77ed578dc3e8076b6ed3754f193ed08750f
+
   </section>
 
 
@@ -89,21 +70,18 @@ export default {
 
 </script>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 466be77ed578dc3e8076b6ed3754f193ed08750f
 <style>
 
-.theme{
+.theme {
   background-color: rgb(18, 50, 255);
   color: white;
 }
-<<<<<<< HEAD
+
 #LangButton {
   margintop: 1px;
   marginright: 2px;
-=======
+}
+
 .format{
   background-color: rgb(18, 50, 255);
   color: white;
@@ -116,7 +94,6 @@ export default {
 #LangButton{
   margintop: 10 px;
   marginright: 10 px;
->>>>>>> 466be77ed578dc3e8076b6ed3754f193ed08750f
   position: absolute;
   top: 20px;
   right: 20px;
