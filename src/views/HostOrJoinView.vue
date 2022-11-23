6fdd5f58c2fe5212a0f3569109d35671301df95b
felixmaathz <94930466+felixmaathz@users.noreply.github.com>
2022-11-23 23:22:51 +0100

    Added grid layout createView

--- src/views/HostOrJoinView.vue
+++ src/views/HostOrJoinView.vue
@@ -3,12 +3,12 @@
   <div style="margin: 2em">
     <router-link v-bind:to="'/'"><button>Back</button></router-link>
   </div>
-    <div>
+  <div>
 
-      <router-link v-bind:to="'/gamemode/'"><button>Host</button></router-link>
-      <br>
-      <router-link v-bind:to="'/clientjoingame/'"><button>Join</button></router-link>
-    </div>
+    <router-link v-bind:to="'/gamemode/'"><button>Host</button></router-link>
+    <br>
+    <router-link v-bind:to="'/clientjoingame/'"><button>Join</button></router-link>
+  </div>
   </body>
 </template>
 
