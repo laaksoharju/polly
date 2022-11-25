b30e6dc05bbb03b381d3b24038a2a672d5528b58
felixmaathz <94930466+felixmaathz@users.noreply.github.com>
2022-11-25 16:56:34 +0100

    Added quiz component for SelectSavedGameView

---
+++ src/components/QuizComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<script>
+
+
+export default {
+
+  name: "QuizComponent",
+  props: {
+    quiz: Object
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
