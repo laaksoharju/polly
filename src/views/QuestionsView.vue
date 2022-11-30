a2dbcf4d23dfa6fe3cc9204605a13d3b3ca566d7
ebbaolivia99 <olivia.hogstedt@gmail.com>
2022-11-30 11:34:18 +0100

    frågevy

--- src/views/QuestionsView.vue
+++ src/views/QuestionsView.vue
@@ -1,33 +1,40 @@
 <template>
-    <div>
+  <body>
+    <div id="questionDesign">
         <h1>{{uiLabels.createQuestions}}</h1>
       <div>
-        {{uiLabels.question}}:
-        <input type="text" v-model="question">
+        <div id="newQst">
+          {{uiLabels.question}}:
+          <input type="text" v-for="(_, i) in question"
+              v-model="question[i]"
+              v-bind:key="'question'+i">
+          <button class="addQstBtn" v-on:click="addQuestion">
+            Add question
+          </button>
+        </div>
         <div>
           Answers:
           <input v-for="(_, i) in answers" 
                  v-model="answers[i]" 
                  v-bind:key="'answer'+i">
           <button v-on:click="addAnswer">
-            Add question
+            Add answer
           </button>
         </div>
       </div>
-      <button v-on:click="addQuestion">
-        Add question
-      </button>
       <input type="number" v-model="questionNumber">
-      <button v-on:click="runQuestion">
+      <button class="runQstBtn" v-on:click="runQuestion">
         Run question
       </button>
       {{data}}
       <router-link v-bind:to="'/result/'+pollId">Check result</router-link>
     </div>
+  </body>
   </template>
   
   <script>
-  import io from 'socket.io-client';
+  //import QuestionComponent from '@/components/QuestionComponent.vue';
+import io from 'socket.io-client';
   const socket = io();
   
   export default {
@@ -36,7 +43,7 @@
       return {
         lang: "",
         pollId: "",
-        question: "",
+        question: ["",""],
         answers: ["", ""],
         questionNumber: 0,
         data: {},
@@ -58,19 +65,38 @@
 
     methods: {
       addQuestion: function () {
-        socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )
+        this.question.push("")
+        /*socket.emit("addQuestion", {pollId: this.pollId, q: this.question, a: this.answers } )*/
+        /*den ovan kanske behövs senare för att koppla till resultview!*/ 
       },
       addAnswer: function () {
-        this.answers.push("");
+        this.answers.push("")
       },
       runQuestion: function () {
         socket.emit("runQuestion", {pollId: this.pollId, questionNumber: this.questionNumber})
       },
-      //createQuestions: function () {
-       // socket.emit("createQuestions", {pollId: this.pollId, question: question})
-      //}
     }
   }
 
   </script>
+
+  <style>
+
+body {
+    background-color: pink;
+    width: 100%;
+    display: grid;
+    grid-template-columns: 30em auto;
+    min-height: 100vh;
+  }
+
+
+  #questionDesign{
+    font-family: "Fjord one" ;
+
+  }
+
+  
+
+</style>
   
