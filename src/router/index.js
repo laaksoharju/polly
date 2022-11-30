df7c6b997e1825f67a2faaa873b5767692ccc390
ebbaolivia99 <olivia.hogstedt@gmail.com>
2022-11-30 12:06:34 +0100

    typSjunde

--- src/router/index.js
+++ src/router/index.js
@@ -31,14 +31,14 @@ const routes = [
     name: 'ConnectView',
     component: () => import(/* webpackChunkName: "about" */ '../views/ConnectView.vue')
   },
-  {
-    path: '/join/:lang',
-    name: 'JoinView',
+  //{
+    //path: '/join/:lang',
+    //name: 'JoinView',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/JoinView.vue')
-  },
+    //component: () => import(/* webpackChunkName: "about" */ '../views/JoinView.vue')
+  //},
   
   {
     path: '/result/:id',
